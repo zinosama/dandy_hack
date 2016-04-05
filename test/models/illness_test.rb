be8f114ac803e296e624936841be7267b1c90349
Zino <rhu5@u.rochester.edu>
2016-04-04 20:24:36 -0400

    add more model tests for illness

--- test/models/illness_test.rb
+++ test/models/illness_test.rb
@@ -13,4 +13,15 @@ class IllnessTest < ActiveSupport::TestCase
 		@illness.name = ""
 		assert_not @illness.valid?
 	end
+
+	test 'name should not be too long' do
+		@illness.name = "a" * 101
+		assert_not @illness.valid?
+	end
+
+	test 'name should be unique' do
+		dup = Illness.new(name: @illness.name)
+		@illness.save
+		assert_not dup.valid?
+	end
 end
